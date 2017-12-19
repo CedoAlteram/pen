8883ad20c3e564501bf70060d3e006497e70e954
varUP <aaron.reyna@gmail.com>
2017-12-18 21:48:27 -0800

    Added bhnet.py

--- TCP_UDP/TCP1.py
+++ TCP_UDP/TCP1.py
@@ -1,19 +1,22 @@
+#! /usr/bin/env python
+
 import socket
 
-target_host = "www.google.com"
-target_port = 80
+TCP_IP = '127.0.0.1'
+TCP_PORT = 9999
+BUFFER_SIZE = 1024
+MESSAGE = 'Hello, you sweet, sweet bastard'
 
 #create a socket object, socket.AF_INET says we are going to use a standard IPv4 address or hostname
 #SOCK_STREAM indicates that this will be a TCP client
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #connect the client
-client.connect((target_host, target_port))
+client.connect((TCP_IP, TCP_PORT))
 
 #send some data
-client.send("GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send(MESSAGE)
+data = client.recv(BUFFER_SIZE)
+client.close()
 
-#receive some data
-response = client.recv(4096)
-
-print response
+print 'received data:', data
--- TCP_UDP/UDP1.py
+++ TCP_UDP/UDP1.py
@@ -1,7 +1,10 @@
+#! /usr/bin/env python
 import socket
 
 target_host = "127.0.0.1"
-target_port = 80
+target_port = 5005
+buffer_size = 1024
+message = "Hello, you sweet, sweet bastard"
 
 #create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
